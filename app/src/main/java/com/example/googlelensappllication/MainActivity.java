20f4ffd58ea6d87b2895562b2c0d30fdd6759e68
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-06-10 22:17:23 +0600

    day3

--- app/src/main/java/com/example/googlelensappllication/MainActivity.java
+++ app/src/main/java/com/example/googlelensappllication/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.googlelensappllication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView caputureIV;
+    private Button snapBtn,getSearchResultsBtn;
+    private RecyclerView resultsRV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        caputureIV=findViewById(R.id.image);
+        snapBtn=findViewById(R.id.BtnSnap);
+        getSearchResultsBtn=findViewById(R.id.BtnResults);
+        resultsRV=findViewById(R.id.RVSearchResult);
+
     }
 }
