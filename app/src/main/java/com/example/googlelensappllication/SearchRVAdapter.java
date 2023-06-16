d1a5bab10b12e280e5be1b03fa6b451fb9cafeec
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-06-16 16:14:41 +0600

    day 5

--- app/src/main/java/com/example/googlelensappllication/SearchRVAdapter.java
+++ app/src/main/java/com/example/googlelensappllication/SearchRVAdapter.java
@@ -1,32 +1,53 @@
 package com.example.googlelensappllication;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class SearchRVAdapter extends RecyclerView.Adapter<SearchRVAdapter.Viewholder> {
+
+    private Context context;
+    private ArrayList<SearchRVModel> searchRVModels;
+
+    public SearchRVAdapter(Context context, ArrayList<SearchRVModel> searchRVModels) {
+        this.context = context;
+        this.searchRVModels = searchRVModels;
+    }
 
     @NonNull
     @Override
     public SearchRVAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(context).inflate(R.layout.search_results_rv_item,parent,false);
+        return new Viewholder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull SearchRVAdapter.Viewholder holder, int position) {
-
+        SearchRVModel searchRVModel=searchRVModels.get(position);
+        holder.titleTV.setText(searchRVModel.getTitle());
+        holder.descTV.setText(searchRVModel.getDisplayed_link());
+        holder.snipperTV.setText(searchRVModel.getSnippet());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return searchRVModels.size();
     }
 
     public class Viewholder extends RecyclerView.ViewHolder{
+        private TextView titleTV,snipperTV,descTV;
         public Viewholder(@NonNull View itemView) {
             super(itemView);
+            titleTV=itemView.findViewById(R.id.TVTitle);
+            snipperTV=itemView.findViewById(R.id.TVSnippet);
+            descTV=itemView.findViewById(R.id.TVDescription);
         }
     }
 }
